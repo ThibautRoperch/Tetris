8a69204944890fd4da1711b0d872819d369fb063
ThibautRoperch <thibaut.roperch@free.fr>
2017-09-11 15:05:39 +0200

    Collisions ok

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tetris
+
+## Documentation
+
+https://fr.wikipedia.org/wiki/Tetris
+https://fr.wikibooks.org/wiki/Tetris/Le_jeu_en_d%C3%A9tail
+http://www.colinfahey.com/tetris/tetris_diagram_pieces_orientations_new.jpg
--- game/game.js
+++ game/game.js
@@ -18,8 +18,8 @@ var TIME = 0; // seconds
 var UNITE = 20; // pixels
 var WELL = document.getElementsByTagName("well")[0];
 var NEXT = document.getElementsByTagName("next")[0];
-var NB_COLUMNS = 10;
-var NB_ROWS = 22;
+var COLUMNS_NB = 10;
+var ROWS_NB = 22;
 
 /**
  * Back-end properties
@@ -35,23 +35,27 @@ var SQUARE_ID = 0; // A.I.
  * Classes
  */
 
- /**
-  * Piece representation, with a name, a structure, a relative origin and the absolute position of her origin
-  */
-function Piece(name, structure, squares_nb, origin, position, rotation) {
-	this.name = name;
-	this.structure = structure;
-	this.squares_nb = squares_nb;
-	this.origin = origin;
-	this.position = position;
-	this.rotation = rotation;
+/**
+ * Piece representation, with a name, a structure, a relative origin and the absolute position of her origin
+ */
+class Piece {
+	constructor(name, structure, squares_nb, origin, position, rotation) {
+		this.name = name;
+		this.structure = structure;
+		this.squares_nb = squares_nb;
+		this.origin = origin;
+		this.position = position;
+		this.rotation = rotation;
+	}
 }
 
- /**
-  * Square representation, with an id corresponding with the id's HTML square
-  */
-function Square(id) {
-	this.id = id;
+/**
+ * Square representation, with an id corresponding with the id's HTML square
+ */
+class Square {
+	constructor(id) {
+		this.id = id;
+	}
 }
 
 
@@ -59,21 +63,21 @@ function Square(id) {
  * Game functions
  */
 
- /**
-  * Main function
-  * Set the well's dimensions for the front-end, matrix's dimensions for the back-end, and launch + prepare the first pieces
-  */
+/**
+ * Main function
+ * Set the well's dimensions for the front-end, matrix's dimensions for the back-end, and launch + prepare the first pieces
+ */
 function launchGame() {
 	// Set well's dimensions
-	WELL.style.width = UNITE * NB_COLUMNS + "px";
-	WELL.style.height = UNITE * NB_ROWS + "px";
+	WELL.style.width = UNITE * COLUMNS_NB + "px";
+	WELL.style.height = UNITE * ROWS_NB + "px";
 	// Set next's dimensions
 	NEXT.style.width = UNITE * 5 + "px";
 	NEXT.style.height = UNITE * 4 + "px";
 	// Set matrix's dimensions
-	for (var r = 0; r < NB_ROWS; ++r) {
+	for (var r = 0; r < ROWS_NB; ++r) {
 		var row = [];
-		for (var c = 0; c < NB_COLUMNS; ++c) {
+		for (var c = 0; c < COLUMNS_NB; ++c) {
 			row.push(null);
 		}
 		MATRIX.push(row);
@@ -91,17 +95,18 @@ function launchGame() {
 function gameClock() {
 	setTimeout(function() {
 		// Check for collision if the current piece moves down
-		// If the piece can move down, move down the current piece
-		// Else, launch a new piece
+		// If the piece can move down, move down the current piece and actualize the display
+ 		// Else, block the piece at her position and launch a new piece
 		if (currentPieceCanMoveDown()) {
 			CURRENT_PIECE.position[1]++;
 			actualizeCurrentPiecePosition();
 		} else {
+			blockCurrentPiece();
 			launchPiece();
 		}
 		// Continue the game
 		gameClock();
-	}, 1000/SPEED);
+	}, 1000 / SPEED);
 }
 
 /**
@@ -217,7 +222,7 @@ function generatePiece() {
 		origin = [1, 0];
 	}
 	
-	var position_x = Math.round(NB_COLUMNS / 2); // default x position of the piece's origin
+	var position_x = Math.round(COLUMNS_NB / 2); // default x position of the piece's origin
 	var position_y = 0; // default y position of the piece's origin
 	var rotation = 0; // default rotation of the piece
 
@@ -242,13 +247,13 @@ function displayPiece(piece, node) {
 			if (piece.structure[r][c] == 0) {
 				square.className = "invisible";
 			} else {
-				square.id = SQUARE_ID++;				
+				square.id = parseInt(SQUARE_ID++);				
 			}
 			tetrimino.appendChild(square);
 		}
 	}
 
-	SQUARE_ID -= tetrimino.squares_nb; // the square id counter will be incremented by another function during the matrix modification
+	SQUARE_ID -= piece.squares_nb; // the square id counter will be incremented by another function during the matrix modification
 
 	node.appendChild(tetrimino);
 }
@@ -263,12 +268,28 @@ function actualizeCurrentPiecePosition() {
 }
 
 /**
- * Return true if the current moving piece can move down, false else
- * Update the matrix if the piece can't move
+ * Update the matrix with each square of the current piece (the piece is blocked at her position)
+ */
+function blockCurrentPiece() {
+	// Check if each square in the piece's structure can move down, relative to the piece's position in the well
+	for (r = 0; r < CURRENT_PIECE.structure.length; ++r) {
+		for (c = 0; c < CURRENT_PIECE.structure[r].length; ++c) {
+			if (CURRENT_PIECE.structure[r][c] == 1) {
+				// Compute the absolute position of the square
+				// absolute position of the origin of the piece + position of the square in the piece - position of the origin in the piece
+				var absolute_square_position = [CURRENT_PIECE.position[0] + c - CURRENT_PIECE.origin[0], CURRENT_PIECE.position[1] + r - CURRENT_PIECE.origin[1]];
+				// Add 1 to the vertical position of the square (like a down move)
+				MATRIX[absolute_square_position[1]][absolute_square_position[0]] = new Square(SQUARE_ID++);
+				console.log("ajout d'un sq en " + absolute_square_position);
+			}
+		}
+	}
+}
+
+/**
+ * Return true if the current moving piece can move down in the well, false else
  */
 function currentPieceCanMoveDown() {
-	var can_move_down = true;
-	
 	// Check if each square in the piece's structure can move down, relative to the piece's position in the well
 	for (r = 0; r < CURRENT_PIECE.structure.length; ++r) {
 		for (c = 0; c < CURRENT_PIECE.structure[r].length; ++c) {
@@ -278,20 +299,66 @@ function currentPieceCanMoveDown() {
 				var absolute_square_position = [CURRENT_PIECE.position[0] + c - CURRENT_PIECE.origin[0], CURRENT_PIECE.position[1] + r - CURRENT_PIECE.origin[1]];
 				// Add 1 to the vertical position of the square (like a down move)
 				absolute_square_position[1]++;
-				// Block the square (and add him to the matrix) if the bottom limit of the well is reached,
-				// or if there already is a square in this position in the matrix,
-				// or if a square of this piece has been blocked
-				if (absolute_square_position[1] == NB_ROWS || MATRIX[absolute_square_position[1]][absolute_square_position[0]] != null || !can_move_down) {
-					can_move_down = false;
-					// Update the matrix if the piece can't move (the piece is blocked at her position)
-					absolute_square_position[1]--;
-					MATRIX[absolute_square_position[1]][absolute_square_position[0]] = new Square(SQUARE_ID++);
+				// Return false if the bottom limit of the well is reached,
+				// or if there already is a square in this position in the matrix
+				if (absolute_square_position[1] == ROWS_NB || MATRIX[absolute_square_position[1]][absolute_square_position[0]] != null) {
+					return false;
 				}
 			}
 		}
 	}
 
-	return can_move_down;
+	return true;
+}
+
+/**
+ * Return true if the current moving piece can move left in the well, false else
+ */
+function currentPieceCanMoveLeft() {
+	// Check if each square in the piece's structure can move left, relative to the piece's position in the well
+	for (r = 0; r < CURRENT_PIECE.structure.length; ++r) {
+		for (c = 0; c < CURRENT_PIECE.structure[r].length; ++c) {
+			if (CURRENT_PIECE.structure[r][c] == 1) {
+				// Compute the absolute position of the square
+				// absolute position of the origin of the piece + position of the square in the piece - position of the origin in the piece
+				var absolute_square_position = [CURRENT_PIECE.position[0] + c - CURRENT_PIECE.origin[0], CURRENT_PIECE.position[1] + r - CURRENT_PIECE.origin[1]];
+				// Substract 1 to the horizontal position of the square (like a left move)
+				absolute_square_position[0]--;
+				// Return false if the left limit of the well is reached,
+				// or if there already is a square in this position in the matrix
+				if (absolute_square_position[0] == 0 || MATRIX[absolute_square_position[1]][absolute_square_position[0]] != null) {
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Return true if the current moving piece can move right in the well, false else
+ */
+function currentPieceCanMoveRight() {
+	// Check if each square in the piece's structure can move right, relative to the piece's position in the well
+	for (r = 0; r < CURRENT_PIECE.structure.length; ++r) {
+		for (c = 0; c < CURRENT_PIECE.structure[r].length; ++c) {
+			if (CURRENT_PIECE.structure[r][c] == 1) {
+				// Compute the absolute position of the square
+				// absolute position of the origin of the piece + position of the square in the piece - position of the origin in the piece
+				var absolute_square_position = [CURRENT_PIECE.position[0] + c - CURRENT_PIECE.origin[0], CURRENT_PIECE.position[1] + r - CURRENT_PIECE.origin[1]];
+				// Add 1 to the horizontal position of the square (like a right move)
+				absolute_square_position[0]++;
+				// Return false if the right limit of the well is reached,
+				// or if there already is a square in this position in the matrix
+				if (absolute_square_position[0] == COLUMNS_NB || MATRIX[absolute_square_position[1]][absolute_square_position[0]] != null) {
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
 }
 
 /**
@@ -301,15 +368,17 @@ function keyPressed(event) {
 	switch (event.key) {
 		case "ArrowLeft": // horizontal move
 			// Move left the current piece if possible
-			// if (currentPieceCanMoveLeft()) {
+			if (currentPieceCanMoveLeft()) {
 				CURRENT_PIECE.position[0]--;
 				actualizeCurrentPiecePosition();
-			// }
+			}
 			break;
 		case "ArrowRight": // horizontal move
 			// Move right the current piece if possible
-			CURRENT_PIECE.position[0]++;
-			actualizeCurrentPiecePosition();
+			if (currentPieceCanMoveRight()) {
+				CURRENT_PIECE.position[0]++;
+				actualizeCurrentPiecePosition();
+			}
 			break;
 		case "ArrowUp": // hard drop
 			// Move to the closer square the current piece
@@ -324,6 +393,9 @@ function keyPressed(event) {
 			if (currentPieceCanMoveDown()) {
 				CURRENT_PIECE.position[1]++;
 				actualizeCurrentPiecePosition();
+			} else {
+				blockCurrentPiece();
+				launchPiece();
 			}
 			break;
 		case "a": // clockwise rotation
